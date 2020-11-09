7b4d4a86460a85e4c8cfcc228db3db191a753dc1
Ryan Beatty <rbeatty1196@gmail.com>
2020-11-08 19:09:22 -0600

    Attempted Dijkstras

--- PA4/Dijkstra.java
+++ PA4/Dijkstra.java
@@ -6,6 +6,19 @@ public class Dijkstra extends Graph {
 		super(graph);
 	}
 
-	public int[] execute(int source) { // complete this method
+	public int[] execute(int source) { 
+		int[] distance = new int[numVertices];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		boolean[] closed = new boolean[numVertices];
+		Arrays.fill(closed, false);
+		PriorityQueue<Integer> open = new PriorityQueue<Integer>();
+		open.setPriority(source, 0);
+		distance[source] = 0;
+		while(open.getSize() != 0) {
+			int minVertex = open.getMinimumItem();
+			open.deleteMinimum();
+			closed[minVertex] = true;
+			//Not Finished
+		}
 	}
 }
