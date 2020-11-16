548e666a01d8756f0aeba1a7dea8bf8e083d4a04
Unknowncall <zharvey3131@gmail.com>
2020-11-16 17:07:01 -0600

    Finished LIS

--- PA4/DynamicProgramming.java
+++ PA4/DynamicProgramming.java
@@ -1,50 +1,69 @@
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DynamicProgramming {
 
-	public static int numberOfBinaryStringsWithNoConsecutiveOnes(int n) { // complete this method
-		int a[] = new int [n], b[] = new int [n];
-		a[0] = b[0] = 1;
+    public static int numberOfBinaryStringsWithNoConsecutiveOnes(int n) { // complete this method
+        int a = 1, b = 1;
 
-		for (int i = 1; i < n; i++) {
-			a[i] = a[i-1] + b[i-1];
-			b[i] = a[i-1];
-		}
+        for (int i = 1; i < n; i++) {
+            int tmp = a;
+            a = a + b;
+            b = tmp;
+        }
 
-		return a[n-1] + b[n-1];
-	}
+        return a + b;
+    }
 
-	public static ArrayList<Integer> longestIncreasingSubsequence(int[] arr, int len) { // complete this method
-		int[] LIS = new int[-1], PRED = new int[-1];
-		int maxIndex;
-		for (int i = 0; i < len -1; i++) {
-			LIS[i] = 1;
-			PRED[i] = -1;
-			// something here...
-		}
+    public static ArrayList<Integer> longestIncreasingSubsequence(int[] arr, int len) { // complete this method
+        int lis[] = new int[len], pred[] = new int[len];
+        for (int i = 0; i < len; i++) {
+            lis[i] = 1;
+            pred[i] = i;
+        }
 
-		int lisIndex = -1;
-		for (int i : LIS) {
-			if (lisIndex < i || lisIndex == -1) {
-				lisIndex = i;
-			}
-		}
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    if (lis[j] + 1 > lis[i]) {
+                        lis[i] = lis[j] + 1;
+                        pred[i] = j;
+                    }
+                }
+            }
+        }
 
-		ArrayList<Integer> solution = new ArrayList<>();
-		// Starting from the lisIndex and by using the PRED array, add the values in the longest increasing subsequence to the dynamic array.
-		reverse(solution);
-		return solution;
-	}
+        int maxIndex = 0;
+        for (int i = 0; i < len; i++) {
+            if (lis[i] > lis[maxIndex]) {
+                maxIndex = i;
+            }
+        }
 
-	private static void reverse(ArrayList<Integer> list) {
-		for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
-			int temp = list.get(j);
-			list.set(j, list.get(i));
-			list.set(i, temp);
-		}
-	}
+        ArrayList<Integer> solution = new ArrayList<>();
+        int tmp = maxIndex;
+        solution.add(arr[tmp]);
+        int tmp2 = pred[tmp];
+
+        while (tmp != tmp2) {
+            tmp = tmp2;
+            solution.add(arr[tmp]);
+            tmp2 = pred[tmp];
+        }
+
+        reverse(solution);
+        return solution;
+    }
+
+    private static void reverse(ArrayList<Integer> list) {
+        for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
+            int temp = list.get(j);
+            list.set(j, list.get(i));
+            list.set(i, temp);
+        }
+    }
 }
