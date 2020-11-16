ad3696fd7af5fb5c8a782ed66d2d20959b67869d
Ryan Beatty <rbeatty1196@gmail.com>
2020-11-15 18:15:38 -0600

    Finished BellmanFord

--- PA4/BellmanFord.java
+++ PA4/BellmanFord.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 
 public class BellmanFord extends Graph {
 
@@ -15,9 +15,32 @@ public class BellmanFord extends Graph {
 			didDistChange = false;
 			for(int j = 0; j < numVertices; j++) {
 				for(int k = 0; k < adjList.get(j).size(); k++) {
-					//Not Finished
+					Edge e = adjList.get(j).get(k);
+					if(dist[e.src] == Integer.MAX_VALUE) {
+						continue;
+					}
+					int newDist = dist[e.src] + e.weight;
+					if(newDist < dist[e.dest]) {
+						dist[e.dest] = newDist;
+						didDistChange = true;
+					}
+				}
+			}
+			if(!didDistChange) {
+				return dist;
+			}
+		}
+		for(int j = 0; j < numVertices; j++) {
+			for(int k = 0; k < adjList.get(j).size(); k++) {
+				Edge e = adjList.get(j).get(k);
+				if(dist[e.src] == Integer.MAX_VALUE) {
+					continue;
+				}
+				if(dist[e.src] + e.weight < dist[e.dest]) {
+					return null;
 				}
 			}
 		}
+		return dist;
 	}
 }
